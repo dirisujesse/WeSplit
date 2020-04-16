1b527073fd53e766207b55ff035a24881ce677b0
Jesse Dirisu <dirisujesse@gmail.com>
2020-04-16 22:26:10 +0100

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Dirisu on 16/04/2020.
+//  Copyright © 2020 Dirisu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
